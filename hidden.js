51f1a0dda54650d4f9fcd7d52a95d3d5e2d24a8c
PierreJzql <pierrejezequel92@gmail.com>
2023-05-04 17:03:06 +0200

    ajout animation

--- hidden.js
+++ hidden.js
@@ -1,1 +1,18 @@
-document.addEventListener("keypress", (e) => {console.log(e.key)})
+let keyLogger = ""
+let array = []
+
+document.addEventListener("keypress", (e) => {
+    
+    keyLogger+=e.key
+    
+    if (e.key==" " || e.key=="Enter"){
+        array.push(keyLogger)
+        keyLogger=""
+        console.log("array = ",array )
+    }
+})
+
+
+document.addEventListener("click",()=>{
+    
+})
